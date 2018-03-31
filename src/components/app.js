74928e38d70c2dcc3d70ceaf3e61cdce8dfed7d7
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-03-30 23:49:30 -0700

    create logic for sets

--- src/components/app.js
+++ src/components/app.js
@@ -7,30 +7,47 @@ class App extends Component {
 
         this.state = {
             pomodoro: 0,
+            isActive: false,
             isWork: true,
-            work: 25,
-            shortBreak: 5,
-            longBreak: 30,
+            work: .25,
+            shortBreak: .1,
+            longBreak: .5,
+        }
+
+        this.progressTimer = this.progressTimer.bind(this);
+    }
+
+    progressTimer() {
+        if (this.state.isWork) {
+            this.setState({
+                pomodoro: this.state.pomodoro + 1,
+                isWork: false,
+            })
+        } else {
+            if (this.state.pomodoro < 4) {
+                this.setState({
+                    isWork: true,
+                });
+            } else {
+                this.setState({
+                    isActive: false,
+                })
+            }
         }
     }
     
     render() {
         return (
+            
             <div>
                 <h1>Pomodoro timer</h1>
-                <Timer seconds={this.state.isWork ? this.state.work * 60 : this.state.shortBreak * 60} />
-                <ul>
-                    <li>work</li>
-                    <li>short break</li>
-                    <li>work</li>
-                    <li>short break</li>
-                    <li>work</li>
-                    <li>short break</li>
-                    <li>work</li>
-                    <li>long break</li>
-                </ul>
+                {this.state.isActive && <Timer progressTimer={this.progressTimer} 
+                    seconds={this.state.isWork ? this.state.work * 60 : 
+                                                 this.state.pomodoro < 4 ? this.state.shortBreak * 60 :
+                                                                           this.state.longBreak * 60} />
+                    }
                 <div className="buttons">
-                    <button>Start Set</button>
+                    <button onClick={() => {this.setState({isActive:true})}}>Start Set</button>
                     <button>Pause</button>
                     <button>Reset Pomodoro</button>
                 </div>
--- src/components/timer.js
+++ src/components/timer.js
@@ -23,16 +23,32 @@ class Timer extends Component {
     }
 
     timer() {
-        this.setState({
-            timer: this.state.timer - 1,
-        })
+        const count = this.state.timer - 1;
+        if(count >= 0) {
+            this.setState({ timer: count })
+        } else {
+            this.props.progressTimer();
+            this.setState({ timer: this.props.seconds})
+        }
     }
 
     render() {
         const minutes = Math.floor((this.state.timer / 60).toString().padStart(2, '0'));
         const seconds = (this.state.timer % 60).toString().padStart(2, '0');
         return (
-            <div>{`${minutes}:${seconds}`}</div>
+            <div className="timer">
+                <div className="display">{`${minutes}:${seconds}`}</div>
+                <ul>
+                    <li>work</li>
+                    <li>short break</li>
+                    <li>work</li>
+                    <li>short break</li>
+                    <li>work</li>
+                    <li>short break</li>
+                    <li>work</li>
+                    <li>long break</li>
+                </ul>
+            </div>
         )
     }
 }
